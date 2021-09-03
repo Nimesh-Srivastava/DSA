e1835b500d6c4c5ce8996f8f662c797bc23a87ce
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2021-09-04 02:05:18 +0530

    Update 0003.cpp

--- C++/0003.cpp
+++ C++/0003.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        vector<int>map1(256, -1);
+        int left = 0, right = 0, n = s.size(), len = 0;
+        
+        while(right < n){
+            
+            if(map1[s[right]] != -1)
+                left = max(map1[s[right]] + 1, left);
+            
+            map1[s[right]] = right;
+            len = max(len, right - left + 1);
+            right++;
+        }
+        
+        return len;
+    }
+};

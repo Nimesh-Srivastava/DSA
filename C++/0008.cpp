1e296e2ac4ef22e3af341a3753d7752d64206234
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2021-09-04 02:10:40 +0530

    Update 0008.cpp

--- C++/0008.cpp
+++ C++/0008.cpp
@@ -1,1 +1,42 @@
-
+class Solution {
+public:
+    int myAtoi(string s) {
+        
+        int len = s.length();
+        int i = 0;
+        
+        if(len == 0)
+            return 0;
+        
+        for(i = 0; i < len && s[i] == ' '; i++);
+        
+        if(i == len)
+            return 0;
+        
+        int sin = 1;
+        
+        if(s[i] == '-'){
+            sin = -1;
+            i = i + 1;
+        }
+        else if(s[i] == '+'){
+            i = i + 1;
+        }
+        
+        long ans = 0;
+        
+        while(i < len && ans < INT_MAX && isdigit(s[i])){
+            ans = ans * 10 + (s[i] - '0');
+            i = i + 1;
+        }
+        
+        if(ans > INT_MAX){
+            if(sin == 1)
+                return INT_MAX;
+            else
+                return INT_MIN;
+        }
+        
+        return ans * sin;
+    }
+};

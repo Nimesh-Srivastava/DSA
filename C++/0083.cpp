381c85b1061017026ef93c538f3112def0f2cceb
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2021-09-03 19:37:58 +0530

    Create 0083.cpp

---
+++ C++/0083.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL)
+            return head;
+        
+        ListNode* temp = head;
+        ListNode* curr = head->next;
+        
+        while(curr != NULL){
+            if(temp->val != curr->val){
+                temp = temp->next;
+            }
+            else{
+                temp->next = curr->next;
+            }
+            
+            curr = curr->next;
+        }
+        
+        return head;
+    }
+};

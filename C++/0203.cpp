82ff4ce6d4c1548127c7fbee9c66ec69ec8b745b
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2021-09-04 10:15:27 +0530

    Update 0203.cpp

--- C++/0203.cpp
+++ C++/0203.cpp
@@ -1,1 +1,38 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        
+        ListNode* temp = head, *prev = NULL;
+        
+        while(temp){
+            
+            if(temp->val == val){
+                
+                if(prev == NULL){
+                    head = temp->next;
+                }   
+                else{
+                    prev->next = temp->next;
+                }
+                
+                temp = temp->next;
+                continue;
+            }
+            
+            prev = temp;
+            temp = temp->next;
+        }
+        
+        return head;
+    }
+};

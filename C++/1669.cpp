43dcef0435462a570bf56563188205890a56cb47
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2021-09-06 21:26:46 +0530

    Update 1669.cpp

--- C++/1669.cpp
+++ C++/1669.cpp
@@ -1,1 +1,37 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        
+        ListNode* head = list1, *tail = list1, *point = list2;
+        
+        a--;
+        
+        while(a--){
+            head = head->next;
+        }
+        
+        while(b--){
+            tail = tail->next;
+        }
+        
+        head->next = list2;
+        
+        while(point->next){
+            point = point->next;
+        }
+        
+        point->next = tail->next;
+        
+        return list1;
+    }
+};

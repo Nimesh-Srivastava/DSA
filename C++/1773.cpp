c9733bd26d1cb814feb5032255193147f2c09ca3
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2021-09-06 20:21:41 +0530

    Update 1773.cpp

--- C++/1773.cpp
+++ C++/1773.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        
+        int check = 0;
+        
+        if(ruleKey == "color")
+            check = 1;
+        else if(ruleKey == "name")
+            check = 2;
+        
+        int result = 0;
+        
+        for(int i=0; i<items.size(); i++){
+            
+            if(items[i][check] == ruleValue)
+                result = result + 1;
+        }
+        
+        return result;
+    }
+};

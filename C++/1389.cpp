e11527a6ef1b20dd755925516aaa119eac3e4771
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2021-09-06 19:26:43 +0530

    Update 1389.cpp

--- C++/1389.cpp
+++ C++/1389.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        
+        vector<int> target;
+        
+        for(int i=0; i<index.size(); i++){
+            target.insert(target.begin() + index[i], nums[i]);
+        }
+        
+        return target;
+    }
+};

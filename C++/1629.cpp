623fdbf51e6e9f45c6613b57cdebbbf8c07277e5
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-09-07 08:01:33 +0530

    Update 1629.cpp

--- C++/1629.cpp
+++ C++/1629.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+        
+        char result = keysPressed[0];
+        
+        int time = releaseTimes[0];
+        
+        for(int i=1; i<keysPressed.size(); i++){
+            
+            if(releaseTimes[i] - releaseTimes[i-1] > time){
+                
+                result = keysPressed[i];
+                time = releaseTimes[i] - releaseTimes[i-1];
+                
+            }
+            else if(time == releaseTimes[i] - releaseTimes[i-1])
+                result = max(result, keysPressed[i]);
+            
+        }
+        
+        return result;
+    }
+};

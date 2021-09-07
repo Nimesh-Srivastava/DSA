c66d927aab0014564b825009f17fe5e1097f650d
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-09-07 12:45:39 +0530

    Update 1814.cpp

--- C++/1814.cpp
+++ C++/1814.cpp
@@ -1,3 +1,4 @@
+//Solution 1 :-
 class Solution {
 public:
     int countNicePairs(std::vector<int>& nums) {
@@ -13,5 +14,41 @@ public:
             map[val]++;
         }
         return count;
+    }
+};
+
+
+//Solution 2 :-
+class Solution {
+public:
+    int countNicePairs(vector<int>& nums) {
+        
+        long long result = 0;
+        
+        unordered_map<long long, int> map1;
+        
+        for(int i=0; i<nums.size(); i++){
+            
+            long long ans = (long long)nums[i] - reverseInt(nums[i]);
+            
+            if(map1.find(ans) != map1.end())
+                result = result + map1[ans];
+            
+            map1[ans]++;
+        }
+        
+        return (result % 1000000007);
+    }
+    
+    long long reverseInt(int num){
+        
+        long long result = 0;
+        
+        while(num){
+            result = (result * 10) + (num % 10);
+            num = num / 10;
+        }
+        
+        return result;
     }
 };

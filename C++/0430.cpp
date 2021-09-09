c1fbb0feb81a41126e2c74d61e7b83de719962eb
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-09-09 16:12:37 +0530

    Update 0430.cpp

--- C++/0430.cpp
+++ C++/0430.cpp
@@ -1,1 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
 
+class Solution {
+public:
+    Node* sol(Node* head){
+        
+        Node* curr = head, *temp = head;
+        
+        while(curr){
+            
+            if(curr->child){
+                Node* tail = sol(curr->child);
+                
+                tail->next = curr->next;
+                if(curr->next)
+                    curr->next->prev = tail;
+                
+                curr->next = curr->child;
+                curr->child->prev = curr;
+                
+                curr->child = NULL;
+                
+                curr = temp;
+            }
+            else
+                curr = curr->next;
+            
+            if(curr)
+                temp = curr;
+        }
+        return temp;
+    }
+    
+    Node* flatten(Node* head) {
+        if(head)
+            sol(head);
+        
+        return head;
+    }
+};

5b658fcd5c34223b3ab43dc55f36219f62752430
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-09-16 18:50:46 +0530

    Create 1656.cpp

---
+++ C++/1656.cpp
@@ -0,0 +1,23 @@
+class OrderedStream {
+public:
+    vector<string> stream;
+    int i;
+    int lim;
+    OrderedStream(int n) {
+        i = 0;
+        lim = n;
+        stream.resize(n);
+    }
+    vector<string> insert(int idKey, string value) {
+        stream[idKey-1] = value;
+        vector<string> out;
+        while(stream[i] != "" && i != lim) out.push_back(stream[i++]);
+        return out;
+    }
+};
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * OrderedStream* obj = new OrderedStream(n);
+ * vector<string> param_1 = obj->insert(idKey,value);
+ */

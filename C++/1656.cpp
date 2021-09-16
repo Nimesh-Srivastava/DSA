f51b1397bc093b75bec4be3c2d09f829f23ea092
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-09-16 18:51:45 +0530

    Update 1656.cpp

--- C++/1656.cpp
+++ C++/1656.cpp
@@ -1,17 +1,22 @@
 class OrderedStream {
 public:
     vector<string> stream;
+    
     int i;
     int lim;
+    
     OrderedStream(int n) {
         i = 0;
         lim = n;
         stream.resize(n);
     }
+    
     vector<string> insert(int idKey, string value) {
         stream[idKey-1] = value;
         vector<string> out;
+        
         while(stream[i] != "" && i != lim) out.push_back(stream[i++]);
+        
         return out;
     }
 };

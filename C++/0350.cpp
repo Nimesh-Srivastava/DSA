63b3104ab81167ff25570df60d7e9260debebbc5
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-09-18 14:09:16 +0530

    Update 0350.cpp

--- C++/0350.cpp
+++ C++/0350.cpp
@@ -1,1 +1,32 @@
-
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
+        unordered_map<int, int> map1;
+        unordered_map<int, int> :: iterator it;
+        
+        vector<int> result;
+        
+        for(auto c : nums1){
+            it = map1.find(c);
+            
+            if(it != map1.end())
+                it->second++;
+            else
+                map1.insert({c, 1});
+        }
+        
+        for(auto c : nums2){
+            it = map1.find(c);
+            
+            if(it != map1.end()){
+                if(it->second > 0){
+                    result.push_back(c);
+                    it->second--;
+                }
+            }
+            
+        }
+        return result;
+    }
+};

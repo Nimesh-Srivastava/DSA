74af054c494578befb2975624f6413fdb706f740
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-09-19 20:44:18 +0530

    Update 1913.cpp

--- C++/1913.cpp
+++ C++/1913.cpp
@@ -1,1 +1,13 @@
+// Time : O(n logn)
 
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        
+        int siz = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        return (nums[siz-2] * nums[siz-1]) - (nums[0] * nums[1]);
+    }
+};

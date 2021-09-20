b804ec76762bf32a661626cb253229d67db046c5
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-09-20 21:34:12 +0530

    Update 1816.cpp

--- C++/1816.cpp
+++ C++/1816.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        
+        int count = 0;
+        string result;
+        
+        for(int i=0; i<s.size(); i++){
+            
+            if(s[i] == ' ')
+                count++;
+            
+            if(count == k && s[i] == ' ')
+                break;
+            
+            result.push_back(s[i]);
+        }
+        
+        return result;
+    }
+};

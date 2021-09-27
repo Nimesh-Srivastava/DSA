1ef821040ed43fedbd1297eb442b106af0ba516c
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-09-27 21:51:56 +0530

    Update 0066.cpp

--- C++/0066.cpp
+++ C++/0066.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+        
+        for(int i=digits.size()-1; i>=0; i--){
+            int sum = digits[i] + carry;
+            carry = sum / 10;
+            digits[i] = sum % 10;
+        }
+        if(carry == 1)
+            digits.insert(digits.begin(), carry);
+        return digits;
+    }
+};

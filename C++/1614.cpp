c9e9055cd6069b5ddc8e25a9aecc89c75de6034d
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-09-30 19:34:05 +0530

    Update 1614.cpp

--- C++/1614.cpp
+++ C++/1614.cpp
@@ -1,1 +1,28 @@
-
+class Solution {
+public:
+    int maxDepth(string s) {
+        
+        stack<int> s1;
+        int count = 0;
+        int max_count = 0;
+        
+        for(int i=0; i<s.size(); i++){
+            
+            if(s[i] == '('){
+                s1.push(s[i]);
+                count++;
+            }
+            
+            else if(s[i] == ')'){
+                
+                if(s1.top() == '('){
+                    max_count = max(max_count, count);
+                    s1.pop();
+                    count--;
+                }
+            }
+        }
+        
+        return max_count;
+    }
+};

6ce317ea70de0837ede482cc75991e335c5a60df
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-01 21:30:11 +0530

    Update 0009.cpp

--- C++/0009.cpp
+++ C++/0009.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        if(x < 0)
+            return false;
+        
+        vector<int> num;
+        
+        while(x != 0){
+            num.push_back(x % 10);
+            x /= 10;
+        }
+        
+        for(int i=0; i<num.size()/2; i++){
+            if(num[i] != num[num.size() - 1 - i])
+                return false;
+        }
+        
+        return true;
+    }
+};

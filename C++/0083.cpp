39ea3911eb4e963dc95d58f91828266375833071
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-02 18:03:52 +0530

    Update 0083.cpp

--- C++/0083.cpp
+++ C++/0083.cpp
@@ -7,7 +7,8 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- */
+ */\
+
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {

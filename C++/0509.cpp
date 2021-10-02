139f8c03f9f3d0c118e413a7d1373d4163a731af
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-03 03:15:43 +0530

    Update 0509.cpp

--- C++/0509.cpp
+++ C++/0509.cpp
@@ -1,1 +1,33 @@
+// solution using recursion
+class Solution {
+public:
+    int fib(int n) {
+        if(n < 2)
+            return n;
+        
+        return fib(n-1) + fib(n-2);
+    }
+};
 
+
+
+// solution using dynamic programming
+class Solution {
+public:
+    int fib(int n) {
+       
+        if(n <= 1)
+            return n;
+        
+        else{
+            int first = 0, second = 1, sum = 0;
+            
+            for(int i=1; i<n; i++){
+                sum = first + second;
+                first = second;
+                second = sum;
+            }
+            return sum;
+        }
+    }
+};

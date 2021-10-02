772d0885b0f6f2ae6ce9b1e3667f854374cfa9f3
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-02 18:04:20 +0530

    Update 0160.cpp

--- C++/0160.cpp
+++ C++/0160.cpp
@@ -3,7 +3,7 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {};
+ *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
 class Solution {

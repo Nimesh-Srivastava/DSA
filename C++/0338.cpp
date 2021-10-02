5c849637e370475d5b3a5c989a0a9db0682acf8a
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-03 02:54:37 +0530

    Update 0338.cpp

--- C++/0338.cpp
+++ C++/0338.cpp
@@ -1,1 +1,19 @@
+// solution using dynamic programming
 
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int> result(n + 1);
+        
+        for(int i=0; i<=n; i++){
+            
+            result[i] = result[i/2];
+            
+            if(i % 2 != 0)
+                result[i] += 1;
+        }
+        
+        return result;
+    }
+};

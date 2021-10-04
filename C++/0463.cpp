a3d00a9168eac5546d0468d61e9d8a75b86a1831
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-04 11:37:07 +0530

    Update 0463.cpp

--- C++/0463.cpp
+++ C++/0463.cpp
@@ -1,3 +1,54 @@
+// solution using dfs
+class Solution {
+public:
+    int dfs(vector<vector<int>>& grid, int i, int j){
+        
+        //if grid ended or water present or cell already visited, return 0
+        if(i<0 || j<0 
+           || i>grid.size()-1 
+           || j>grid[0].size()-1 
+           || !grid[i][j] 
+           || grid[i][j] == 2)
+            return 0;
+        
+        //define visited cell
+        grid[i][j] = 2;
+        
+        //define max perimeter
+        int peri = 4;
+        
+        //if land connected at bottom
+        if(i < grid.size()-1 && grid[i + 1][j])
+            peri--;
+        //if land connected at right
+        if(j < grid[0].size()-1 && grid[i][j + 1])
+            peri--;
+        //if land connected at left
+        if(j > 0 && grid[i][j - 1])
+            peri--;
+        //if land connected at top
+        if(i > 0 && grid[i - 1][j])
+            peri--;
+        
+        //obtain dfs for left, right, top, bottom
+        return peri + (dfs(grid, i, j - 1) + dfs(grid, i, j + 1) + dfs(grid, i - 1, j) + dfs(grid, i + 1, j)); 
+    }
+    
+    int islandPerimeter(vector<vector<int>>& grid) {
+       
+        int result = 0;
+        
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                if(grid[i][j])
+                    return dfs(grid, i, j);
+            }
+        }
+        return result;
+    }
+};
+
+
 //Bruteforce solution
 class Solution {
 public:

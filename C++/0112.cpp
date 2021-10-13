6d02171aed7ac7b7b064ed407737306a476331ed
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-13 23:43:22 +0530

    Update 0112.cpp

--- C++/0112.cpp
+++ C++/0112.cpp
@@ -1,1 +1,36 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool is_path(TreeNode* root, int targetSum, int curr_sum){
+        
+        if(!root)
+            return false;
+        
+        curr_sum += root -> val;
+        
+        if(!root -> left && !root -> right){
+            
+            if(curr_sum == targetSum)
+                return true;
+            
+            else
+                return false;
+        }
+        
+        return (is_path(root -> left, targetSum, curr_sum) || is_path(root -> right, targetSum, curr_sum));
+    }
+    
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        return is_path(root, targetSum, 0);
+    }
+};

fe94b7f3ea963761be6871328bd37e12a3fc6654
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-14 19:26:43 +0530

    Update 0700.cpp

--- C++/0700.cpp
+++ C++/0700.cpp
@@ -1,1 +1,32 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* dfs(TreeNode* root, int val){
+        
+        if(!root)
+            return NULL;
+        
+        if(root -> val == val)
+            return root;
+        
+        if(root -> val < val)
+            return dfs(root -> right, val);
+        
+        else
+            return dfs(root -> left, val);
+    }
+    
+    TreeNode* searchBST(TreeNode* root, int val) {
+        return dfs(root, val);
+    }
+};

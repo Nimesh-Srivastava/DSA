bdcad7630ba08b7931752149a32e913f784f6b8d
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-15 17:00:45 +0530

    Create 0098.cpp

---
+++ C++/0098.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& result){
+        
+        if(!root)
+            return;
+        
+        inorder(root -> left, result);
+        
+        result.push_back(root -> val);
+        
+        inorder(root -> right, result);
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        
+        vector<int> result;
+        
+        inorder(root, result);
+        
+        for(int i = 0; i < result.size() - 1; i++)
+            if(result[i] >= result[i + 1])
+                return false;
+        
+        return true;
+    }
+};

90f62e4010c929a97a04be5fc82c57a38a27f911
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-18 21:25:52 +0530

    Update 0993.cpp

--- C++/0993.cpp
+++ C++/0993.cpp
@@ -1,1 +1,55 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    
+    int count = 0;
+    int parent = 0;
+    
+public:
+    void depth(TreeNode* root, int val, int cnt, TreeNode* prnt){
+        
+        if(!root)
+            return;
+        
+        if(root -> val == val){
+            count = cnt;
+            parent = prnt -> val;
+            if(count == 0)
+                return;
+        }
+        
+        cnt++;
+        
+        depth(root -> left, val, cnt, root);
+        depth(root -> right, val, cnt, root);
+    }
+    
+    bool isCousins(TreeNode* root, int x, int y) {
+        
+        int d_x = 0, p_x = 0;
+        int d_y = 0, p_y = 0;
+        
+        depth(root, x, 0, root);
+        d_x = count;
+        p_x = parent;
+        
+        depth(root, y, 0, root);
+        d_y = count;
+        p_y = parent;
+        
+        if(d_x == d_y && p_x != p_y)
+            return true;
+        
+        else
+            return false;
+    }
+};

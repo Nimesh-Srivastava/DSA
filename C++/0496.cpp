52648d95ddaa608fa7d995aad567e65e4095fa8d
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-19 21:44:43 +0530

    Update 0496.cpp

--- C++/0496.cpp
+++ C++/0496.cpp
@@ -38,3 +38,45 @@ public:
 
 
 //Solution 2 : Optimized using stacks
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        
+        //vector to store next greater elements of nums2
+        vector<int> next_gr(nums2.size());
+        
+        stack<int> st;
+        st.push(0);
+        
+        //find next greater elements of nums2
+        for(int i=1; i<nums2.size(); i++){
+            
+            while(!st.empty() && nums2[i] > nums2[st.top()]){
+                
+                next_gr[st.top()] = nums2[i];
+                st.pop();
+            }
+            
+            st.push(i);
+        }
+        
+        //store -1 whose next greater element is not present
+        while(!st.empty()){
+            
+            next_gr[st.top()] = -1;
+            st.pop();
+        }
+        
+        //mapping for element-to-index call
+        unordered_map<int, int> map1;
+        for(int i=0; i<nums2.size(); i++)
+            map1[nums2[i]] = i;
+        
+        vector<int> result;
+        
+        for(int i=0; i<nums1.size(); i++)
+            result.push_back(next_gr[map1[nums1[i]]]);
+        
+        return result;
+    }
+};

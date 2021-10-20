828a0a6b5263c01b686fe86f86d3866feeae737d
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-20 19:52:19 +0530

    Update 0151.cpp

--- C++/0151.cpp
+++ C++/0151.cpp
@@ -1,1 +1,36 @@
-
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+        string result;
+        string word;
+        
+        int siz = s.length();
+        int i = 0;
+        
+        while(i < siz){
+            
+            while(i < siz && s[i] == ' ')
+                i++;
+            
+            if(i >= siz)
+                break;
+            
+            int j = i + 1;
+            
+            while(j < siz && s[j] != ' ')
+                j++;
+            
+            word = s.substr(i, j - i);
+            
+            if(result.length() == 0)
+                result = word;
+            else
+                result = word + " " + result;
+            
+            i = j + 1;
+        }
+        
+        return result;
+    }
+};

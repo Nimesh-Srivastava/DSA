5a84cfe037bc8fac7fe77d1e1ed569f9ab92f557
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-24 00:40:00 +0530

    Update 0154.cpp

--- C++/0154.cpp
+++ C++/0154.cpp
@@ -1,3 +1,4 @@
+//Solution 1:-
 class Solution {
 public:
     int findMin(vector<int>& nums) {
@@ -9,5 +10,32 @@ public:
         }
         
         return minVal;
+    }
+};
+
+
+//Solution 2:-
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while(left < right){
+            
+            int mid = left + (right - left)/2;
+            
+            if(nums[mid] > nums[right])
+                left = mid + 1;
+            
+            else if(nums[mid] < nums[right])
+                right = mid;
+            
+            else
+                right--;
+        }
+        
+        return nums[left];
     }
 };

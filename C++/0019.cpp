0037c3e432cc68e1f86449cb68815ce8286e0da8
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-26 11:46:37 +0530

    Update 0019.cpp

--- C++/0019.cpp
+++ C++/0019.cpp
@@ -1,3 +1,4 @@
+//Solution 1 :-
 class Solution {
 public:
   ListNode *removeNthFromEnd(ListNode *head, int n) {
@@ -20,4 +21,30 @@ public:
     delete start;
     return head;
   }
+};
+
+
+//Solution 2 :-
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        for(int i=0; i<n; i++)
+            fast = fast -> next;
+        
+        if(!fast)
+            return head -> next;
+        
+        while(fast -> next){
+            fast = fast -> next;
+            slow = slow -> next;
+        }
+        
+        slow -> next = slow -> next -> next;
+        
+        return head;
+    }
 };

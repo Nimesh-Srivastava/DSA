6cbe8d5e4d6d022a908f90bc1bea9d4c85cc638d
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-31 15:30:30 +0530

    Create 0413.cpp

---
+++ C++/LeetCode/0413.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        
+        if(nums.size() < 3)
+            return 0;
+        
+        vector<int> dp(nums.size(), 0);
+        
+        int sum = 0;
+        
+        for(int i = 2; i < nums.size(); i++){
+            if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]){
+                dp[i] = dp[i - 1] + 1;
+                sum += dp[i];
+            }
+        }
+        
+        return sum;
+    }
+};

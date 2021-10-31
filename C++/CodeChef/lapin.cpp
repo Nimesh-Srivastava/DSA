dcc26cb816f8a8235c92c1759a087552f2db942e
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-10-31 22:01:14 +0530

    Update lapin.cpp

--- C++/CodeChef/lapin.cpp
+++ C++/CodeChef/lapin.cpp
@@ -1,3 +1,4 @@
+//Solution 1 :-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -39,4 +40,52 @@ int main() {
 	}
 	
 	return 0;
+}
+
+
+//Solution 2 :-
+//This solution is with arrays only
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string cal(string& s){
+    int half = s.length() / 2;
+
+    vector<int> freq(26, 0);
+
+    for(int i = 0; i < half; i++)
+        freq[s[i] - 'a']++;
+
+    if(s.length() % 2 != 0)
+        half++;
+
+    for(int i = half; i < s.length(); i++)
+        freq[s[i] - 'a']--;
+        
+    for(int i = 0; i < 26; i++){
+        if(freq[i] != 0)
+            return "NO";
+    }
+    return "YES";
+}
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+
+        string s;
+        cin >> s;
+
+        string ans = cal(s);
+        cout << ans << endl;
+    }
+
+    return 0;
 }

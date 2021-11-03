6f6ca186c81b93368b2d1a4143c634555eb39a4f
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-11-03 16:08:54 +0530

    Update 0143.cpp

--- C++/LeetCode/0143.cpp
+++ C++/LeetCode/0143.cpp
@@ -1,3 +1,4 @@
+//Solution 1 :-
 class Solution {
 public:
   void reorderList(ListNode *head) {
@@ -24,4 +25,41 @@ public:
       slow = temp;
     }
   }
+};
+
+
+//Solution 2 :-
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if(!head || !head -> next || !head -> next -> next)
+            return;
+        
+        stack<ListNode*> st;
+        
+        ListNode* curr = head;
+        
+        int siz = 0;
+        
+        while(curr){
+            st.push(curr);
+            
+            siz++;
+            
+            curr = curr -> next;
+        }
+        
+        curr = head;
+        
+        for(int i = 0; i < siz/2; i++){
+            ListNode* temp = st.top();
+            st.pop();
+            
+            temp -> next = curr -> next;
+            curr -> next = temp;
+            curr = curr -> next -> next;
+        }
+        
+        curr -> next = NULL;
+    }
 };

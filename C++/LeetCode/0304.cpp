a6f76f502dcfa989cfc63de2c7c33ca2d2de67d1
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-11-04 17:31:12 +0530

    Create 0304.cpp

---
+++ C++/LeetCode/0304.cpp
@@ -0,0 +1,27 @@
+class NumMatrix {
+    
+    vector<vector<int>> dp;
+    
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        dp = vector<vector<int>>(m + 1, vector<int>(n + 1, 0));
+        
+        for(int i = 1; i < m + 1; i++)
+            for(int j = 1; j < n + 1; j++)
+                dp[i][j] = matrix[i - 1][j - 1] 
+                + dp[i - 1][j] 
+                + dp[i][j - 1] 
+                - dp[i - 1][j - 1];
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return (dp[row1][col1]
+               + dp[row2 + 1][col2 + 1]
+               - dp[row1][col2 + 1]
+               - dp[row2 + 1][col1]);
+    }
+};

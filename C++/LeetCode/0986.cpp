5155b3a919721bcb800bc009cbdcc20f17989926
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-11-24 22:02:47 +0530

    Create 0986.cpp

---
+++ C++/LeetCode/0986.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<vector<int>> ans;
+        int i = 0, j = 0;
+        
+        while(i < size(A) && j < size(B)) {
+            
+            if(A[i][1] >= B[j][0] && A[i][0] <= B[j][1]) 
+                ans.push_back({max(A[i][0], B[j][0]), min(A[i][1], B[j][1])});
+            
+            A[i][1] < B[j][1] ? i++ : j++;
+        }
+        
+        return ans;
+    }
+};

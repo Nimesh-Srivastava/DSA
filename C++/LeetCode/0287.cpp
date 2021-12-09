03416862553736da287b21601c712d4b10e3e775
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-12-09 20:05:50 +0530

    Update 0287.cpp

--- C++/LeetCode/0287.cpp
+++ C++/LeetCode/0287.cpp
@@ -26,3 +26,22 @@ public:
         return NULL;
     }
 };
+
+//Solution 3 :- (Most Optimal)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        for(int i = 0; i < nums.size(); i++){
+            
+            int index = abs(nums[i]) - 1;
+            
+            nums[index] *= -1;
+            
+            if(nums[index] > 0)
+                return abs(nums[i]);
+        }
+        
+        return NULL;
+    }
+};

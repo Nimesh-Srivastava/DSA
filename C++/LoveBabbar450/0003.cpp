afb39bad74924a135731ea3e239753ae94f1297e
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-12-10 15:54:45 +0530

    Create 0003.cpp

---
+++ C++/LoveBabbar450/0003.cpp
@@ -0,0 +1,16 @@
+//Solution 1 :-
+int kthSmallest(int arr[], int l, int r, int k) {
+        vector<int> nums;
+        
+        for(int i = 0; i <= r; i++){
+            int temp = arr[i];
+            nums.push_back(temp);
+        }
+            
+        sort(nums.begin(), nums.end());
+        
+        return nums[k - 1];
+}
+
+
+//Solution 2 :-

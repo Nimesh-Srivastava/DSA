4d0245980eaab6e125e62c76b58e7b133a5939f8
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-01-20 18:39:46 +0530

    Update 0034.cpp

--- C++/LeetCode/0034.cpp
+++ C++/LeetCode/0034.cpp
@@ -1,3 +1,4 @@
+//Solution 1 :-
 class Solution {
 public:
   vector<int> searchRange(vector<int> &arr, int target) {
@@ -32,4 +33,41 @@ public:
     }
     return {lb, ub};
   }
+};
+
+//Solution 2 :-
+class Solution {
+public:
+    int bSearch(vector<int>& nums, int target, string pos){
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        int ans = -1;
+        
+        while(l <= r){
+            
+            int mid = l + (r - l) / 2;
+            
+            if(nums[mid] == target){
+                ans = mid;
+                
+                if(pos == "one")
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+            
+            else if(nums[mid] > target)
+                r = mid - 1;
+            
+            else
+                l = mid + 1;
+        }
+        
+        return ans;
+    }
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {bSearch(nums, target, "one"), bSearch(nums, target, "two")};   
+    }
 };

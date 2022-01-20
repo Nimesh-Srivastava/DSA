afd80a447289af66871022ce145435e2714e3432
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-01-20 19:09:55 +0530

    Update 0033.cpp

--- C++/LeetCode/0033.cpp
+++ C++/LeetCode/0033.cpp
@@ -1,3 +1,4 @@
+//Solution 1 :-
 class Solution {
 public:
   int search(vector<int> &arr, int target) {
@@ -22,4 +23,42 @@ public:
     }
     return -1;
   }
+};
+
+//Solution 2 :-
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        int ans = -1;
+        
+        while(l <= r){
+            
+            int mid = l + (r - l) / 2;
+            
+            if(nums[mid] == target){
+                ans = mid;
+                break;
+            }
+            
+            else if(nums[mid] >= nums[l]){
+                if(target >= nums[l] && target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+            
+            else{
+                if(target > nums[mid] && target <= nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
 };

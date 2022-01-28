69d8b7054504a42e77c82690f7f641d236668ff5
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-01-28 21:30:23 +0530

    Update 0078.cpp

--- C++/LeetCode/0078.cpp
+++ C++/LeetCode/0078.cpp
@@ -1,3 +1,4 @@
+//Solution 1 :-
 class Solution {
 public:
   void backtrack(vector<int> &arr, vector<vector<int>> &ans, vector<int> curr,
@@ -16,4 +17,33 @@ public:
     backtrack(arr, ans, curr);
     return ans;
   }
+};
+
+
+//Solution 2 :-
+class Solution {
+public:
+    vector<vector<int>> result;
+    
+    void recur(vector<int>& nums, vector<int> temp, int siz){
+        
+        if(siz == nums.size()){
+            result.push_back(temp);
+            return;
+        }
+        
+        recur(nums, temp, siz + 1);
+        temp.push_back(nums[siz]);
+        recur(nums, temp, siz + 1);
+        
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<int> temp;
+        
+        recur(nums, temp, 0);
+        
+        return result;
+    }
 };

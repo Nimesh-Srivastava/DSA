7d6388e21c02b0013ab3277b9adede3dd651b39e
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-01-29 19:10:30 +0530

    Update 0039.cpp

--- C++/LeetCode/0039.cpp
+++ C++/LeetCode/0039.cpp
@@ -1,3 +1,4 @@
+//Solution 1 :-
 class Solution {
 public:
   void backtrack(vector<int> &arr, int target, vector<vector<int>> &ans,
@@ -20,4 +21,34 @@ public:
     backtrack(arr, target, ans, curr);
     return ans;
   }
+};
+
+
+//Solution 2 :-
+class Solution {
+public:
+    
+    vector<vector<int>>result;
+    vector<int>current;
+
+void function(vector<int>& candidates,int target,int index)
+{
+    if(target==0){
+        result.push_back(current);
+        return;
+    }
+    
+    if(index==candidates.size() || target<0)return;
+    
+    current.push_back(candidates[index]);
+    function(candidates,target-candidates[index],index);
+    current.pop_back();
+    function(candidates,target,index+1);      
+}
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        function(candidates,target,0);
+        return result;
+    }
 };

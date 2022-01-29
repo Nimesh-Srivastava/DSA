89f4108f542db2ba66edf82c79527015dec3b6ab
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-01-30 00:18:28 +0530

    Update 0040.cpp

--- C++/LeetCode/0040.cpp
+++ C++/LeetCode/0040.cpp
@@ -1,3 +1,4 @@
+//Solution 1 :-
 class Solution {
 public:
   void backtrack(vector<int> &arr, int target, vector<vector<int>> &ans,
@@ -23,4 +24,53 @@ public:
     backtrack(arr, target, ans, curr);
     return ans;
   }
+};
+
+
+//Solution 2 :-
+class Solution {
+public:
+    void backtrack(vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& temp, int pos, int siz){
+        
+        if(target == 0){
+            ans.push_back(temp);
+            return;
+        }
+        
+        if(pos == siz)
+            return;
+        
+        int init = pos;
+        
+        while(pos < siz){
+            
+            if(pos != init && candidates[pos] == candidates[pos - 1]){
+                pos++;
+                continue;
+            }
+            
+            if(candidates[pos] <= target){
+                temp.push_back(candidates[pos]);
+                backtrack(candidates, target - candidates[pos], ans, temp, pos + 1, siz);
+                temp.pop_back();
+            }
+            else
+                break;
+            
+            pos++;
+        }
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        
+        vector<vector<int>> result;
+        vector<int> temp;
+        int siz = candidates.size();
+        
+        sort(candidates.begin(), candidates.end());
+        
+        backtrack(candidates, target, result, temp, 0, siz);
+        
+        return result;
+    }
 };

447785e1a7952b6be146dda52737d4e5b4afe036
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-03-09 19:38:23 +0530

    Create 0799.cpp

---
+++ C++/LeetCode/0799.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        
+        double v[101][101] = {0.0};
+
+        v[0][0] = poured;
+    
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (v[i][j] >= 1) {
+                    
+                    v[i + 1][j] += (v[i][j] - 1) / 2.0;
+                    
+                    v[i + 1][j + 1] += (v[i][j] - 1) / 2.0;
+                    
+                    v[i][j] = 1;
+                }
+            }
+        }
+    
+        return v[query_row][query_glass];
+    }
+};

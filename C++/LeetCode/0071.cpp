e64d978881c90da3a2cc9f0dbf817f944a396a72
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-03-14 21:50:46 +0530

    Update 0071.cpp

--- C++/LeetCode/0071.cpp
+++ C++/LeetCode/0071.cpp
@@ -1,3 +1,4 @@
+//Solution 1 :-
 class Solution {
 public:
   string simplifyPath(string path) {
@@ -24,4 +25,48 @@ public:
     }
     return ans;
   }
+};
+
+
+//Solution 2 :-
+class Solution {
+public:
+    string simplifyPath(string path) {
+        
+        stack<string> st;
+        string res;
+        
+        for(int i = 0;  i<path.size(); ++i) {
+            if(path[i] == '/')    
+                continue;
+            
+            string temp;
+            
+            while(i < path.size() && path[i] != '/') {
+                temp += path[i];
+                i++;
+            }
+            
+            if(temp == ".")
+                continue;
+			
+            else if(temp == "..") {
+                if(!st.empty())
+                    st.pop();
+            }
+            
+            else
+                st.push(temp);
+        }
+        
+        while(!st.empty()) {
+            res = "/" + st.top() + res;
+            st.pop();
+        }
+        
+        if(res.size() == 0)
+            return "/";
+        
+        return res;
+    }
 };

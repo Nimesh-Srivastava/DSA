959868c9af69775b0bb29ac3cda41ec45216a0ba
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-05-27 18:28:18 +0530

    Create 1342.cpp

---
+++ C++/LeetCode/1342.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        
+        int cnt = 0;
+        
+        while(num) {
+            
+            if(num & 1)
+                num--;
+            
+            else
+                num = num >> 1;
+            
+            cnt++;
+        }
+        
+        return cnt;
+    }
+};

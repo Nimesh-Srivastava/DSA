513b30c58880a135d6bebc50de0955e02ee65d78
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-06-15 13:46:29 +0530

    Create 1048.cpp

---
+++ C++/LeetCode/1048.cpp
@@ -0,0 +1,27 @@
+class Solution {
+
+    static bool compare(const string &s1, const string &s2) {
+        return s1.length() < s2.length();
+    }
+    
+public:
+    int longestStrChain(vector<string>& words) {
+        
+        sort(words.begin(), words.end(), compare);
+        
+        unordered_map<string, int> dp;
+        
+        int res = 0;
+        
+        for (string& w : words) {
+            for (int i = 0; i < w.length(); i++) {
+                string pre = w.substr(0, i) + w.substr(i + 1);
+                dp[w] = max(dp[w], dp.find(pre) == dp.end() ? 1 : dp[pre] + 1);
+            }
+            
+            res = max(res, dp[w]);
+        }
+        
+        return res;
+    }
+};

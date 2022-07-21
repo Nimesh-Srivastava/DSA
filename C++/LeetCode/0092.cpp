d78b8762697cabc8de83d474c6d64262d8a8fed8
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-07-21 10:48:58 +0530

    Update 0092.cpp

--- C++/LeetCode/0092.cpp
+++ C++/LeetCode/0092.cpp
@@ -1,3 +1,31 @@
+// Solution 1 :-
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        
+        ListNode* dummy = new ListNode(0);
+        dummy -> next = head;
+        
+        ListNode* prev = dummy;
+        
+        for(int i = 0; i < left - 1; i++)
+            prev = prev -> next;
+        
+        ListNode* curr = prev -> next;
+        
+        for(int i = 0; i < right - left; i++) {
+            ListNode* temp = curr -> next;
+            curr -> next = temp -> next;
+            temp -> next = prev -> next;
+            prev -> next = temp;
+        }
+        
+        return dummy -> next;
+    }
+};
+
+
+// Solution 2 :-
 class Solution {
 public:
   ListNode *reverseBetween(ListNode *head, int left, int right) {

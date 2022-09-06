6b757b5b7420c043afb55a509b3e009c1c4ef385
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-09-06 09:02:43 +0530

    Update 0814.cpp

--- C++/LeetCode/0814.cpp
+++ C++/LeetCode/0814.cpp
@@ -1,3 +1,4 @@
+// Solution 1 :-
 class Solution {
 public:
   TreeNode *pruneTree(TreeNode *root) {
@@ -9,4 +10,21 @@ public:
       return root;
     return nullptr;
   }
+};
+
+// Solution 2 :-
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if (!root)
+            return NULL;
+        
+        root -> left = pruneTree(root -> left);
+        root -> right = pruneTree(root -> right);
+        
+        if (!root -> left && !root -> right && root -> val == 0)
+            return NULL;
+        
+        return root;
+    }
 };

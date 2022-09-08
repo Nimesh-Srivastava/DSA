1f90258d8322dadfc2f89d48510ddf52ab0444da
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-09-08 09:45:37 +0530

    Update 0094.cpp

--- C++/LeetCode/0094.cpp
+++ C++/LeetCode/0094.cpp
@@ -1,14 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Solution 1 :-
 class Solution {
 public:
     void in_order(TreeNode* root, vector<int>& values){
@@ -30,5 +20,29 @@ public:
         in_order(root, result);
         
         return result;
+    }
+};
+
+
+// Solution 2 :-
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        stack<TreeNode*> st;
+        
+        while (root || !st.empty()) {
+            while (root) {
+                st.push(root);
+                root = root -> left;
+            }
+            
+            root = st.top();
+            st.pop();
+            
+            nodes.push_back(root -> val);
+            root = root -> right;
+        }
+        return nodes;
     }
 };

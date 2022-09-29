285b34a9e435b5ccb0503d71057edece9f6e3cd8
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-09-29 10:55:24 +0530

    Create 0658.cpp

---
+++ C++/LeetCode/0658.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        
+        int l = 0;
+        int r = arr.size() - k;
+        
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            
+            if (x - arr[mid] > arr[mid + k] - x)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return vector<int>(arr.begin() + l, arr.begin() + l + k);
+    }
+};
